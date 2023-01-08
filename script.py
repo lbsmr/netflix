92b3dbf3372f3904e30fa9ff3f1cc08d9cce50ea
lbsmr <lorenzobernardoserge@gmail.com>
2023-01-08 12:52:59 -0300

    Update

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output
 import pycountry
+import dash_bootstrap_components as dbc
 
 ## Get each country 3 letter code
 def get_alpha_3(location):
@@ -11,7 +12,7 @@ def get_alpha_3(location):
     except:
         return 'None'
 
-app = Dash(__name__,assets_folder='./assets')
+app = Dash(__name__,external_stylesheets=[dbc.themes.COSMO])
 df = pd.read_csv('./netflix-rotten-tomatoes-metacritic-imdb.csv')
 
 ## Death Note has two movies with the same title, and messed up charts
@@ -86,14 +87,13 @@ actors['avg'] = a_avgs
 pie_1 = px.pie(pie_i,values='count',names='type')
 pie_1.update_layout(
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)',
-    font=dict(family = 'Arial',color = '#F8F8FF')
+    plot_bgcolor='rgba(0,0,0,0)'
 )
 
 ## Languages pie chart
 pie_2 = px.pie(languages,values='count',names='language')
 pie_2.update_traces(textposition='inside')
-pie_2.update_layout(uniformtext_minsize=12, uniformtext_mode='hide',paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)',font=dict(family = 'Arial',color = '#F8F8FF'),)
+pie_2.update_layout(uniformtext_minsize=12, uniformtext_mode='hide',paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
 
 ## Release dates chart
 fig3 = go.Figure(
@@ -111,7 +111,6 @@ fig3.update_layout(
     autosize = True,
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)',
-    font=dict(family = 'Arial',color = '#F8F8FF'),
     margin=dict(l=5, r=0, t=25, b=0)
 )
 
@@ -127,7 +126,6 @@ g_graph.update_layout(
     showlegend=False,
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)',
-    font=dict(family = 'Arial',color = '#F8F8FF'),
     margin=dict(l=0, r=0, t=0, b=0),
 )    
 g_graph.add_annotation(
@@ -145,105 +143,122 @@ c_graph = px.choropleth(
     hover_name='name',
     labels={'count':'Content available'},
     color_continuous_scale=px.colors.sequential.Peach_r,
-    width= 1450,
-    height=600
+    height = 550
 )
 c_graph.update_geos(resolution=50,showocean=True, oceancolor="LightBlue",)
 c_graph.update_layout(
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)',
-    font=dict(family = 'Arial',color = '#F8F8FF')
+    plot_bgcolor='rgba(0,0,0,0)'
 )
 
 
-app.layout = html.Div(children=[
-    html.H1("Netflix dataset analysis", className='title'),
-    html.H2("Series and Movies distribution"),
-    html.Div(children = [
-        html.Div(className = 'static_graph', children = [
-            dcc.Graph(figure=pie_1,config={'displayModeBar': False})
-        ]),
-    ]),
-    html.H2("Content available on each country"),
-    html.Div(children = [
-        html.Div(className = 'static_graph', children = [
-            dcc.Graph(figure=c_graph,config={'displayModeBar': False})
-        ]),
-    ]),
-    html.H2("Languages available"),
-    html.Div(children = [
-        html.Div(className = 'static_graph', children = [
-            dcc.Graph(figure=pie_2,config={'displayModeBar': False})
-        ]),
-    ]),
-    html.H2("Release date of content"),
-    html.Div(children = [
-        html.Div(className = 'static_graph', children = [
-            dcc.Graph(figure=fig3,config={'displayModeBar': False})
-        ]),
-    ]),
-    html.H2("Genres on Netflix"),
-    html.Div(children = [
-        html.Div(className = 'static_graph', children = [
-            dcc.Graph(figure=g_graph,config={'displayModeBar': False})
-        ]),
-    ]),
-    html.Div(style={'margin-bottom':420} ,children = [
-        html.H2("Top rated movies by genre"),
-        html.Div(className = 'left_content' , children=[
-                html.H4('Select genre'),
-                dcc.Dropdown(id="g_slct",
-                                options=[{'label':x,'value':x} for x in genres['index']],
-                                multi=False,
-                                value="Drama",
-                                className='dropdown'
-                ),
-                html.H4('Select platform'),
-                dcc.Dropdown(id="app_slct",
-                                options=[
-                                    {'label':'IMDb','value':'IMDb Score'},
-                                    {'label':'Metacritic','value':'Metacritic Score'},
-                                    {'label':'Rotten Tomatoes','value':'Rotten Tomatoes Score'},
-                                    {'label':'Hidden Gem Score (Netflix)','value':'Hidden Gem Score'}
-                                ],
-                                multi=False,
-                                value="IMDb Score",
-                                className='dropdown'
-                ),
-                html.H4('Select type'),
-                dcc.Dropdown(id="t_slct",
-                                options=[
-                                    {'label':'All','value':'All'},
-                                    {'label':'Movies only','value':'Movie'},
-                                    {'label':'Series only','value':'Series'}
-                                ],
-                                multi=False,
-                                value="All",
-                                className='dropdown'
-                ),
-        ]),
-        html.Div(className = 'right_content' , children = [
-            dcc.Graph(id='graph1', figure={},config={'displayModeBar': False})
-        ]),
-    ]),
-    html.Div(children = [
-        html.H2("Crew members appearances on Netflix and average IMDb rating"),
-        html.Div(className = 'left_content' , children=[
-                html.H4('Select crew member'),
-                dcc.Dropdown(id="c_slct",
-                                options=[
-                                    {'label':'Directors','value':'Director'},
-                                    {'label':'Actors','value':'Actor'},
-                                    {'label':'Writers','value':'Writer'}
-                                ],
-                                multi=False,
-                                value="Director",
-                                className='dropdown'
-                )
-        ]),
-        html.Div(className = 'right_content' , children = [
-            dcc.Graph(id='graph2', figure={},config={'displayModeBar': False})
-        ]),
+app.layout = dbc.Container(children=[
+    html.H1("Netflix dataset analysis", className= 'p-6 mt-2'),
+    dbc.Row(align = 'center', children = [
+        dbc.Tabs(id = 'tabs', active_tab = 'series_movies', children = [
+            dbc.Tab(label = 'Series and movies', tab_id = 'series_movies', children = [
+                html.H2("Series and Movies distribution", className = 'p-3'),
+                html.Div(children = [
+                    html.Div(className = 'static_graph', children = [
+                        dcc.Graph(figure=pie_1,config={'displayModeBar': False})
+                    ]),
+                ]),
+            ]),
+            dbc.Tab(label = 'Content per country', children = [
+                html.H2("Content available on each country", className = 'p-3'),
+                html.Div(children = [
+                    html.Div(className = 'static_graph', children = [
+                        dcc.Graph(figure=c_graph,config={'displayModeBar': False})
+                    ]),
+                ]),
+            ]),
+            dbc.Tab(label = 'Languages available', children = [
+                html.H2("Languages available", className = 'p-3'),
+                html.Div(children = [
+                    html.Div(className = 'static_graph', children = [
+                        dcc.Graph(figure=pie_2,config={'displayModeBar': False})
+                    ]),
+                ]),
+            ]),
+            dbc.Tab(label = 'Release date', children = [
+                html.H2("Release date of content",className = 'p-3'),
+                html.Div(children = [
+                    html.Div(className = 'static_graph', children = [
+                        dcc.Graph(figure=fig3,config={'displayModeBar': False})
+                    ]),
+                ]),
+            ]),
+            dbc.Tab(label = 'Genres', children = [
+                html.H2("Genres on Netflix", className = 'p-3'),
+                html.Div(children = [
+                    html.Div(className = 'static_graph', children = [
+                        dcc.Graph(figure=g_graph,config={'displayModeBar': False})
+                    ]),
+                ]),
+            ]),
+            dbc.Tab(label = 'Rating by genre', children = [
+                dbc.Row(children = [
+                    html.H2("Top rated movies by genre", className = 'p-3'),
+                    dbc.Col(md = 3, children = [
+                        html.Div(children=[
+                                html.H4('Select genre', className= 'p-1'),
+                                dcc.Dropdown(id="g_slct",
+                                                options=[{'label':x,'value':x} for x in genres['index']],
+                                                multi=False,
+                                                value="Drama"
+                                ),
+                                html.H4('Select platform', className= 'p-1'),
+                                dcc.Dropdown(id="app_slct",
+                                                options=[
+                                                    {'label':'IMDb','value':'IMDb Score'},
+                                                    {'label':'Metacritic','value':'Metacritic Score'},
+                                                    {'label':'Rotten Tomatoes','value':'Rotten Tomatoes Score'},
+                                                    {'label':'Hidden Gem Score (Netflix)','value':'Hidden Gem Score'}
+                                                ],
+                                                multi=False,
+                                                value="IMDb Score"
+                                ),
+                                html.H4('Select type', className= 'p-1'),
+                                dcc.Dropdown(id="t_slct",
+                                                options=[
+                                                    {'label':'All','value':'All'},
+                                                    {'label':'Movies only','value':'Movie'},
+                                                    {'label':'Series only','value':'Series'}
+                                                ],
+                                                multi=False,
+                                                value="All"
+                                ),
+                        ]),
+                    ]),
+                    dbc.Col(md = 9, children = [
+                        html.Div(children = [
+                            dcc.Graph(id='graph1', figure={},config={'displayModeBar': False})
+                        ]),
+                    ])
+                ])
+            ]),
+            dbc.Tab(label = 'Crew count and average rating', children = [
+                dbc.Row(children = [
+                    html.H2("Crew members appearances on Netflix and average IMDb rating", className = 'p-3'),
+                    dbc.Col(md = 3, children = [
+                        dcc.Dropdown(id="c_slct",
+                                        options=[
+                                            {'label':'Directors','value':'Director'},
+                                            {'label':'Actors','value':'Actor'},
+                                            {'label':'Writers','value':'Writer'}
+                                        ],
+                                        multi=False,
+                                        value="Director"
+                        )
+                    ]),
+                    dbc.Col(md = 9, children = [
+                        html.Div(children = [
+                            dcc.Graph(id='graph2', figure={},config={'displayModeBar': False})
+                        ]),
+                    ])
+                ])
+            ])
+        ])
     ])
 ])
 
@@ -281,7 +296,7 @@ def update_graph1(g_slct,app_slct,t_slct):
         x=app_slct,
         y = 'Title',
         color_discrete_sequence=px.colors.sequential.Peach_r,
-        width=1100,
+        width=1000,
         height=400,
         orientation='h',
     )
@@ -292,7 +307,6 @@ def update_graph1(g_slct,app_slct,t_slct):
         autosize = True,
         paper_bgcolor='rgba(0,0,0,0)',
         plot_bgcolor='rgba(0,0,0,0)',
-        font=dict(family = 'Arial',color = '#F8F8FF'),
         margin=dict(l=5, r=0, t=25, b=0),
     )
 
@@ -340,7 +354,6 @@ def update_graph2(c_slct):
         autosize = True,
         paper_bgcolor='rgba(0,0,0,0)',
         plot_bgcolor='rgba(0,0,0,0)',
-        font=dict(family = 'Arial',color = '#F8F8FF'),
         margin=dict(l=5, r=0, t=25, b=0)
     )
 
